a62bc9142fc899be71ce4c78a316828cece08be5
Faiyazmahmud75 <67873766+Faiyazmahmud75@users.noreply.github.com>
2023-07-18 21:24:11 +0600

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,32 @@
+
+$(function (){
+    $(window).scroll(function(){
+        let myScreen = $(this).scrollTop();
+        if (myScreen > 400){
+            $('.header').addClass('scroll-effect');
+        } 
+        else{
+            $('.header').removeClass('scroll-effect');
+        }
+    });
+});
+// venoBox plugin initialization
+
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'bounce'
+});
+
+// back to top button visiblity function 
+
+window.addEventListener('scroll', function() {
+    let myScreen = window.scrollY;
+    if (myScreen > 500){
+        document.querySelector(".back-to-top").style.display = "block";
+    } else {
+        document.querySelector(".back-to-top").style.display = "none";
+    }
+});
